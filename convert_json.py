194e81e417ec04e6d17c045a8dc855e06951f981
Matthieu <matthieunoel69136@gmail.com>
2023-04-21 13:37:03 +0200

    Ok ça marche

---
+++ convert_json.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+input_file = "./fr_FR.txt"
+output_file = "./fr_FR.json"
+
+# check if output file exists and delete it if it does
+if os.path.exists(output_file):
+    os.remove(output_file)
+
+# read input file
+with open(input_file, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+# create JSON array
+data = {"data": []}
+for line in lines:
+    array = line.strip().replace("\n", "").split("	")
+    array[1] = array[1].replace(",", " ").replace(" ", " ").replace("ː", " ").replace("̃", " ").replace("ʼ", " ")
+    if not ' ' in array[0]:
+        data["data"].append(array)
+
+# write output file
+with open(output_file, "w", encoding="utf-8") as f:
+    json.dump(data, f)
